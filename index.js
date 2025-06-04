e21503ad80427c14c66a52e503559e2ad79b78d2
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2025-06-04 19:50:00 +0000

    added tags

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-var menu = document.getElementById("menuList");
-document.getElementById("menu").onclick=()=>{
-    menu.style.right=0
+import st from "./lib/st"
+st(window)
+class MenuList extends HTMLElement {
+    constructor() {
+      super();
+    }
+    get enabled(){
+        return !!this.attributes.getNamedItem("enabled")
+    }
+    set enabled(val){
+        if(val == this.enabled)return;
+        if (val){
+            var att=new Attr();
+            att.name="enabled"
+            this.attributes.setNamedItem(att);
+        }else{
+            this.attributes.removeNamedItem("enabled")
+        }
+        var ce = new CustomEvent("enabledChanged",{bubbles:true});
+        ce.elm=this;
+        ce.enabled=val;
+        this.dispatchEvent(ce);
+    }
+    connectedCallback(){
+
+    }
+    // Element functionality written in here
+}
+customElements.define("menulist",MenuList)
+class MenuItem extends HTMLElement {
+    isButton=false;
+    constructor() {
+      super();
+    }
+    connectedCallback(){
+
+    }
+}
+customElements.define("menuitem",MenuItem)
+window.onload=()=>{
+    var menu = document.getElementById("mainMenu");
+    document.getElementById("menu").onclick=()=>{
+        menu.enabled=true;
+    }
 }
